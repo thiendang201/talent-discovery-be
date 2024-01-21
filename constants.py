8cf6b19d8f66818999d4670547f8e003a0545bf5
thiendang201 <thiendang201@gmail.com>
2024-01-22 01:35:32 +0700

    update max file size

--- constants.py
+++ constants.py
@@ -3,6 +3,6 @@ RESUMES_BUCKET = "talent-discovery-resumes"
 THUMBNAILS_BUCKET = "talent-discovery-thumbnails"
 
 KB = 1024
-MAX_SIZE_IN_MB = 1024 * 5 * KB
+MAX_SIZE_IN_MB = 1024 * KB
 
 SUPPORTED_FILE_TYPES = {"application/pdf": "pdf"}
